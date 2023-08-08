f7623e897f290f8b4ad0f924c415842dc9a34e07
woonseok105 <yws3786@gmail.com>
2023-08-08 21:34:24 +0900

    Create DateTimetableEntity

---
+++ PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/dateTimetable/persistence/entity/DateTimetableEntity.kt
@@ -0,0 +1,67 @@
+package com.nodoubt.planitbackend.domain.dateTimetable.persistence.entity
+
+import com.nodoubt.planitbackend.domain.teacher.persistence.entity.TeacherEntity
+import com.nodoubt.planitbackend.global.entity.BaseIDEntity
+import org.jetbrains.annotations.NotNull
+import java.sql.Date
+import javax.persistence.*
+
+@Entity
+@Table(name = "tbl_dateTimetable")
+class DateTimetableEntity(
+
+    id: Long,
+
+    year: Int,
+
+    semester: Int,
+
+    date: Date,
+
+    grade: Int,
+
+    classNum: Int,
+
+    period: Int,
+
+    weekOfDate: Int,
+
+    subject: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    val teacherEntity: TeacherEntity
+
+) : BaseIDEntity(id) {
+
+    @Column(columnDefinition = "VARCHAR(4)")
+    @field:NotNull
+    var year = year
+
+    @Column(columnDefinition = "VARCHAR(1)")
+    @field:NotNull
+    var semester = semester
+
+    @field:NotNull
+    var date = date
+
+    @Column(columnDefinition = "VARCHAR(1)")
+    @field:NotNull
+    var grade = grade
+
+    @Column(columnDefinition = "VARCHAR(1)")
+    @field:NotNull
+    var classNum = classNum
+
+    @Column(columnDefinition = "VARCHAR(1)")
+    @field:NotNull
+    var period = period
+
+    @Column(columnDefinition = "VARCHAR(1)")
+    @field:NotNull
+    var weekOfDate = weekOfDate
+
+    @Column(columnDefinition = "VARCHAR(30)")
+    @field:NotNull
+    var subject = subject
+}
