35cc6fb9c5bc7d3c48899f8058c215489b7d1b2f
woonseok105 <yws3786@gmail.com>
2023-08-08 21:34:06 +0900

    Create TeacherEntity

---
+++ PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/teacher/persistence/entity/TeacherEntity.kt
@@ -0,0 +1,22 @@
+package com.nodoubt.planitbackend.domain.teacher.persistence.entity
+
+import com.nodoubt.planitbackend.domain.user.persistence.entity.UserEntity
+import com.nodoubt.planitbackend.global.entity.BaseIDEntity
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.MapsId
+import javax.persistence.OneToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tbl_teacher")
+class TeacherEntity (
+
+    id: Long,
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val userEntity: UserEntity
+
+) : BaseIDEntity(id)
