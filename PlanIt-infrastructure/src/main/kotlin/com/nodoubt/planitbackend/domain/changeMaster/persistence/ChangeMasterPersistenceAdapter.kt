ee28c5ed3410534e1b6b2a5ff7e0e685e2740272
woonseok105 <yws3786@gmail.com>
2023-09-19 22:51:55 +0900

    Add Timetable PersistenceAdapter

---
+++ PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/changeMaster/persistence/ChangeMasterPersistenceAdapter.kt
@@ -0,0 +1,19 @@
+package com.nodoubt.planitbackend.domain.changeMaster.persistence
+
+import com.nodoubt.planitbackend.domain.changeMaster.domain.ChangeMaster
+import com.nodoubt.planitbackend.domain.changeMaster.mapper.ChangeMasterMapper
+import com.nodoubt.planitbackend.domain.changeMaster.spi.ChangeMasterPort
+import com.nodoubt.planitbackend.global.annotation.Adapter
+
+@Adapter
+class ChangeMasterPersistenceAdapter (
+    private val changeMasterRepository: ChangeMasterRepository,
+    private val changeMasterMapper: ChangeMasterMapper
+) : ChangeMasterPort {
+
+    override fun saveChangeMaster(changeMaster: ChangeMaster): Long {
+        val changeMasterEntity = changeMasterRepository.save(changeMasterMapper.toEntity(changeMaster))
+        return changeMasterEntity.id
+    }
+
+}
---
+++ PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/reinforecementClass/persistence/ReinforcementClassAdapter.kt
@@ -0,0 +1,29 @@
+package com.nodoubt.planitbackend.domain.reinforecementClass.persistence
+
+import com.nodoubt.planitbackend.domain.reinforcementClass.domain.ReinforcementClass
+import com.nodoubt.planitbackend.domain.reinforcementClass.spi.ReinforcementClassPort
+import com.nodoubt.planitbackend.domain.reinforecementClass.mapper.ReinforcementClassMapper
+import com.nodoubt.planitbackend.global.annotation.Adapter
+
+@Adapter
+class ReinforcementClassAdapter (
+    private val reinforcementClassRepository: ReinforcementClassRepository,
+    private val reinforcementClassMapper: ReinforcementClassMapper
+) : ReinforcementClassPort {
+
+    override fun saveReinforcementClassList(reinforcementClass: List<ReinforcementClass>) {
+        val reinforcementClassEntities = reinforcementClass.map {
+            reinforcementClassMapper.toEntity(
+                ReinforcementClass(
+                    changeMasterId = it.changeMasterId,
+                    status = it.status,
+                    reinforcementPlan = it.reinforcementPlan,
+                    reasonRejected = it.reasonRejected,
+                    dateTimetableId = it.dateTimetableId,
+                    teacherId = it.teacherId
+                )
+            )
+        }
+        reinforcementClassRepository.saveAll(reinforcementClassEntities)
+    }
+}
