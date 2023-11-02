ffbef2eff054c9b7df39a79f93a56422e705b47b
gilgeunwoo <kesksjxhx@gmail.com>
2023-11-02 22:59:09 +0900

    🦷 :: (#10) root uri를 timetable로 통일

--- PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/replacementClass/presentation/ReplacementClassWebAdapter.kt
+++ PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/replacementClass/presentation/ReplacementClassWebAdapter.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/replacement-class")
+@RequestMapping("/timetables/replacement-class")
 @RestController
 class ReplacementClassWebAdapter(
     private val queryReplacementClassListPort: QueryReplacementClassListPort,
--- PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/semesterTimetable/presentation/SemesterTimetableWebAdapter.kt
+++ PlanIt-infrastructure/src/main/kotlin/com/nodoubt/planitbackend/domain/semesterTimetable/presentation/SemesterTimetableWebAdapter.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/semester-timetable")
+@RequestMapping("/timetables/semester")
 @RestController
 class SemesterTimetableWebAdapter(
     private val querySemesterTimetableIdPort: QuerySemesterTimetableIdPort
